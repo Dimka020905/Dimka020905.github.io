260e994f15637edb8e62e692ef125710097dcf50
Dimka020905 <102362342+Dimka020905@users.noreply.github.com>
2022-03-26 23:36:34 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Шо такоє Україна",
+    answer: "Самое піздатоє шо може буть",
+    options: [
+      "Гівно",
+      "Самое піздатоє шо може буть",
+      "Я шо знаю",
+      "НАТО"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Кто такой путін",
+    answer: "Хуйло",
+    options: [
+      "Ідіот",
+      "Свиня",
+      "Хуйло",
+      "Пєтух"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Что єсть в Українців, но нема в рускіх",
+    answer: "Право на вибір і мозги",
+    options: [
+      "Право на вибір",
+      "Нічо",
+      "Нафта",
+      "Право на вибір і мозги"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Кто спиздив БТР",
+    answer: "Гопніки",
+    options: [
+      "Воєнні",
+      "Америка",
+      "Баба Надя",
+      "Гопніки"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Хто така Свєта",
+    answer: "Топова Україночка",
+    options: [
+      "Топова Україночка",
+      "Сєпаратист",
+      "Тєраріст",
+      "Свєта"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    // {
+    // numb: 6,
+    // question: "Your Question is Here",
+    // answer: "Option 1",
+    // options: [
+    //   "Option 1",
+    //   "option 2",
+    //   "option 3",
+    //   "option 4"
+    // ]
+  // },
+];
